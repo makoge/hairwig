52361b166169ab92fce2b5dc15e4508bc936aa7c
makoge <makogeryan@gmail.com>
2025-09-04 23:54:27 +0300

    first commit

---
+++ hair.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('scroll', 
+         function(){
+          const header = document.querySelector('header');
+          if(window.scrollY > 50) {
+            header.classList.add('scrolled');
+          } else{
+            header.classList.remove('scrolled')
+          }
+         });
+
+
+const toggleBtn = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+      
+ 
